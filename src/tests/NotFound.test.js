537c51204f9a9dbb65b8cb9773b1f27079eb372c
Joao Moreira <jgmoreira1809@gmail.com>
2022-07-18 15:44:33 -0700

    Requisito 4 de novo

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import App from '../App';
 import { NotFound } from '../pages';
 
 const renderWithRouter = (component) => {
