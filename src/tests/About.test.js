af5e5a0c1c23b3f295a84a7d722e2133aa514da7
Joao Moreira <jgmoreira1809@gmail.com>
2022-07-18 12:45:53 -0700

    Requisito 2 - All dead, lieutenant

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,36 @@
-test('oi', () => {});
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { render, screen } from '@testing-library/react';
+import { About } from '../pages';
+
+const renderWithRouter = (component) => {
+  const history = createMemoryHistory();
+  return ({
+    ...render(<Router history={ history }>{component}</Router>), history,
+  });
+};
+
+describe('02. Testa o componente About.js', () => {
+  test('se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+    const pokeText = screen.getByRole('heading', { name: /About Pokédex/i });
+
+    expect(pokeText).toBeDefined();
+  });
+  test('se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+    const firstParagraph = screen.getByText(/This application simulates/i);
+    const secondParagraph = screen.getByText(/One can filter /i);
+
+    expect(firstParagraph).toBeDefined();
+    expect(secondParagraph).toBeDefined();
+  });
+  test('se a página contém contém a seguinte imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+    const pokedexImg = screen.getByRole('img');
+    const imgURL = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
+    expect(pokedexImg.src).toBe(imgURL);
+  });
+});
